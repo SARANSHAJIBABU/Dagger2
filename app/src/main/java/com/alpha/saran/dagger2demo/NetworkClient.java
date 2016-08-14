a2249754170320f4000ab096fad2262b393f1b1e
saran <saranbs2013@gmail.com>
2016-08-14 13:39:56 +0800

    Implemented constructor injection for NetworkClient module

--- app/src/main/java/com/alpha/saran/dagger2demo/NetworkClient.java
+++ app/src/main/java/com/alpha/saran/dagger2demo/NetworkClient.java
@@ -2,11 +2,14 @@ package com.alpha.saran.dagger2demo;
 
 import android.util.Log;
 
+import javax.inject.Inject;
+
 /**
  * Created by saran on 13/8/16.
  */
 public class NetworkClient {
 
+    @Inject
     public NetworkClient(Logger logger){
         if(logger!=null){
             Log.d("Dagger2","Logger created");
